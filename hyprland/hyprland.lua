6551314961d947549c28ca0e408d020d35025f51
tarra <crazydaveangry@gmail.com>
2026-09-26 10:56:14 +0700

    style: tighten inner window gaps

--- hyprland/hyprland.lua
+++ hyprland/hyprland.lua
@@ -4,7 +4,7 @@
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 4,
+        gaps_in  = 5,
         gaps_out = 8,
 
         border_size = 4,
